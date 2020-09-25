82b5cefdbcdb837033be92b7863e0c9974518067
Wally Clewett <wclewett@usc.edu.au>
2020-09-25 14:47:46 +1000

    better logic, less fluff

--- Scripts/ADSync.ps1
+++ Scripts/ADSync.ps1
@@ -3,7 +3,7 @@ Install-Module PSZoom
 Import-Module PSZoom
 Import-Module ActiveDirectory
 
-# API key and secret (https://marketplace.zoom.us/user/build)
+# API key and secret
 $Global:ZoomApiKey    = 'API_Key_Goes_Here'
 $Global:ZoomApiSecret = 'API_Secret_Goes_Here'
 
@@ -15,31 +15,19 @@ $Global:ZoomApiSecret = 'API_Secret_Goes_Here'
  Write-Progress -Activity "Gathering users from Zoom"
  $ZoomUsers = (Get-ZoomUsers -Status active -AllPages) + (Get-ZoomUsers -Status inactive -AllPages)
  
- # Set counter
- $i = 1
- 
- # Sync AD account with Zoom
- ForEach ($ADUser in $ADUsers) {
- 
-     # Set easy to read variables
-     $Email = $ADUser.Mail
-     $IsADEnabled = $ADUser.Enabled
-     $IsZoomUser = $ZoomUsers.email.Contains($Email)
- 
-     # Write status to progress bar
-     Write-Progress -Activity "Syncing users with AD" -Status "$i of $($ADUsers.Count)" -PercentComplete ($i / $($ADUsers.Count) * 100)
-     
-     # Activate Zoom user
-     If ($IsADEnabled -and $IsZoomUser) {
-         Update-ZoomUserStatus -UserId $Email -Action activate # -ApiKey $ZoomApiKey -ApiSecret $ZoomApiSecret
-     }
-     
-     # Deactivate Zoom user
-     If ((-not $IsADEnabled) -and $IsZoomUser) {
-         Update-ZoomUserStatus -UserId $Email -Action deactivate # -ApiKey $ZoomApiKey -ApiSecret $ZoomApiSecret
-     }
- 
-     # Increase counter
-     $i++
- } 
+# Deactivate Zoom users who are disabled in Active Directory
+$ActiveZoomUsers = (Get-ZoomUsers -Status active -AllPages)
+$ActiveZoomUsers.email | ForEach-Object {
+    Get-ADUser -Filter {mail -eq $_ -and enabled -eq $false} -Properties mail} | ForEach-Object {
+        Write-Host "Deactivate $($_.mail)"
+        Update-ZoomUserStatus -UserId $($_.mail) -Action deactivate
+}
+
+# Activate Zoom users who are enabled in Active Directory
+$InactiveZoomUsers = (Get-ZoomUsers -Status inactive -AllPages)
+$InactiveZoomUsers.email | ForEach-Object {
+    Get-ADUser -Filter {mail -eq $_ -and enabled -eq $true} -Properties mail} | ForEach-Object {
+        Write-Host "Enable $($_.mail)"
+        Update-ZoomUserStatus -UserId $($_.mail) -Action activate
+}
  
