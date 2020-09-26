a96a781d2a5c020661d8b95f80fa2ca5cc0c0e38
Wally Clewett <wally.clewett@gmail.com>
2020-09-26 18:11:37 +1000

    Update RebootZoomRooms.ps1

--- Scripts/RebootZoomRooms.ps1
+++ Scripts/RebootZoomRooms.ps1
@@ -5,13 +5,9 @@ Import-Module ActiveDirectory
 # Get all Zoom Room computers
 $ZoomRoomComputers = Get-ADComputer -SearchBase "OU=Zoom Rooms,OU=Shared,OU=Workstations,DC=usc,DC=internal" -Filter *
 
-# Loop through each computer
+# Loop through each computer, test connection and reboot
 ForEach ($ComputerName in $ZoomRoomComputers.Name) {
-    
-    # Test computer is online
     If (Test-Connection -ComputerName $ComputerName -Count 1 -Quiet) {
-        
-        # Restart computer
         Restart-Computer -ComputerName $ComputerName -Force -Verbose -WhatIf
     }
 }
